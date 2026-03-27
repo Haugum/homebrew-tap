09bd5e960d4b4e2a7eda443e7f1200abee9e4b5f
Shellby <shellby@users.noreply.github.com>
2026-03-27 21:39:10 +0700

    Use public tap homepage for flugenberg formula

--- Formula/flugenberg.rb
+++ Formula/flugenberg.rb
@@ -1,6 +1,6 @@
 class Flugenberg < Formula
   desc "Convert EPUB/MOBI/AZW3 books into Kindle-friendly AZW3 and send them over USB"
-  homepage "https://github.com/Haugum/flugenberg"
+  homepage "https://github.com/Haugum/homebrew-tap"
   url "https://github.com/Haugum/homebrew-tap/releases/download/v0.3.1/flugenberg-0.3.1-macos-arm64.tar.gz"
   sha256 "6e58afd59ec9ce6aec938098afcb3a251ac172a522c85a9a65bd8ff7a12eac60"
   version "0.3.1"
